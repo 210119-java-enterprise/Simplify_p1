25b9646e9eedfe8a4fbac5d4e7a6932d69e9552d
wills-designs <william3696@live.com>
2021-02-19 00:18:55 -0500

    created the basic crud implementation of the update method

---
+++ src/main/java/com/revature/repositories/Crud.java
@@ -0,0 +1,10 @@
+package com.revature.repositories;
+
+import java.util.List;
+
+public interface Crud<T> {
+    void save(T newObj);
+    List<T> findAll();
+    T findById(int id);
+    boolean update(T updateObj);
+}
